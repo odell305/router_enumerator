ee58db08c47ead4db512a20fd12fdbf567b17b13
odell305 <odell486@gmail.com>
2019-01-04 16:50:23 -0600

    Setup router enumerator test cases

---
+++ router_enumerator/RouterEnumerator.py
@@ -0,0 +1,5 @@
+class RouterEnumerator:
+
+    def __init__(self):
+        pass
+
---
+++ tests/test_RouterEnumerator.py
@@ -0,0 +1,10 @@
+import unittest
+from router_enumerator.RouterEnumerator import RouterEnumerator
+
+class TestRouterEnumerator(unittest.TestCase):
+
+    def test_instantiate_RouterEnumerator(self):
+        router_enumerator = RouterEnumerator()
+
+    def test_tplink_archer_c8_html_returns_correct_name(self):
+        self.fail()
