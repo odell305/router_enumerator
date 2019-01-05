4fa6353e5bd59fed40bc0cfddade40f80ab31f66
Anthony Michael Pruitt <anthony.m.pruitt@gmail.com>
2019-01-05 12:56:04 -0600

    Remove test_instantiate_RouterEnumerator from tests.

--- tests/test_RouterEnumerator.py
+++ tests/test_RouterEnumerator.py
@@ -12,9 +12,6 @@ TPLINK_ARCHER_C8_HTML = TPLINK_ARCHER_C8_PAGE_FILE.read_text(encoding='UTF-8')
 
 class TestRouterEnumerator(unittest.TestCase):
 
-    def test_instantiate_RouterEnumerator(self):
-        router_enumerator = RouterEnumerator()
-
     def test_tplink_archer_c8_html_returns_correct_name_when_enumerating_from_source(self):
         page_html = TPLINK_ARCHER_C8_HTML
 
