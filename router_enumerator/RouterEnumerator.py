c831c80f45361e04b786519d886d1790e3fbc2da
Anthony Michael Pruitt <anthony.m.pruitt@gmail.com>
2019-01-13 11:51:41 -0600

    Instantiate RouterEnumerator with a database file

--- router_enumerator/RouterEnumerator.py
+++ router_enumerator/RouterEnumerator.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
+from pathlib import Path
 
 driver = webdriver.Firefox()
 
+
 class RouterEnumerator:
 
-    def __init__(self):
+    def __init__(self, routers_db: Path):
+        self.routers_db = routers_db
+        assert self.routers_db.is_file()
+
         self.full_name = None
 
     def enumerate_from_source(self, source: str) -> str:
--- tests/test_RouterEnumerator.py
+++ tests/test_RouterEnumerator.py
@@ -8,6 +8,9 @@ driver = webdriver.Firefox()
 TEST_RESOURCES_DIR = Path('./test_resources/')
 assert TEST_RESOURCES_DIR.is_dir()
 
+ROUTERS_DB_PATH = Path('../resources/routers.db')
+assert ROUTERS_DB_PATH.is_file()
+
 TPLINK_ARCHER_C8_PAGE_FILE = PurePath.joinpath(TEST_RESOURCES_DIR, 'tplink_archer_c8_page')
 assert TPLINK_ARCHER_C8_PAGE_FILE.is_file()
 TPLINK_ARCHER_C8_HTML = TPLINK_ARCHER_C8_PAGE_FILE.read_text(encoding='UTF-8')
@@ -15,10 +18,13 @@ TPLINK_ARCHER_C8_HTML = TPLINK_ARCHER_C8_PAGE_FILE.read_text(encoding='UTF-8')
 
 class TestRouterEnumerator(unittest.TestCase):
 
+    def setUp(self):
+        self.router_enumerator = RouterEnumerator(ROUTERS_DB_PATH)
+
     # This is a test for routers on this specific ip address
     # We assume that the title for the page is "Home"
     def test_192_168_1_254(self):
-        router_enumerator = RouterEnumerator()
+        router_enumerator = RouterEnumerator(ROUTERS_DB_PATH)
         expected_url = "http://192.168.1.254/"
         driver.get("http://192.168.1.254/")
         self.assertEquals(expected_url, "http://192.168.1.254/")
@@ -27,11 +33,10 @@ class TestRouterEnumerator(unittest.TestCase):
     def test_tplink_archer_c8_html_returns_correct_name_when_enumerating_from_source(self):
         page_html = TPLINK_ARCHER_C8_HTML
 
-        router_enumerator = RouterEnumerator()
-        router_enumerator.enumerate_from_source(page_html)
+        self.router_enumerator.enumerate_from_source(page_html)
 
         expected = 'TP-LINK Archer C8'
-        actual = router_enumerator.full_name
+        actual = self.router_enumerator.full_name
 
         self.assertEqual(expected, actual)
 
