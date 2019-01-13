f1b6df7858b989ef9b5063cf6f92da0162c5022f
Anthony Michael Pruitt <anthony.m.pruitt@gmail.com>
2019-01-13 12:48:06 -0600

    Replace tplink_archer_c8 page with arris_nvg589 page for testing

--- tests/test_RouterEnumerator.py
+++ tests/test_RouterEnumerator.py
@@ -11,9 +11,9 @@ assert TEST_RESOURCES_DIR.is_dir()
 ROUTERS_DB_PATH = Path('../resources/routers.db')
 assert ROUTERS_DB_PATH.is_file()
 
-TPLINK_ARCHER_C8_PAGE_FILE = PurePath.joinpath(TEST_RESOURCES_DIR, 'tplink_archer_c8_page')
-assert TPLINK_ARCHER_C8_PAGE_FILE.is_file()
-TPLINK_ARCHER_C8_HTML = TPLINK_ARCHER_C8_PAGE_FILE.read_text(encoding='UTF-8')
+ARRIS_NVG5689 = PurePath.joinpath(TEST_RESOURCES_DIR, 'Arris_NVG589.html')
+assert ARRIS_NVG5689.is_file()
+ARRIS_NVG589_HTML = ARRIS_NVG5689.read_text(encoding='UTF-8')
 
 
 class TestRouterEnumerator(unittest.TestCase):
@@ -29,13 +29,12 @@ class TestRouterEnumerator(unittest.TestCase):
         driver.get("http://192.168.1.254/")
         self.assertEquals(expected_url, "http://192.168.1.254/")
 
-    # Not going to work without this router
-    def test_tplink_archer_c8_html_returns_correct_name_when_enumerating_from_source(self):
-        page_html = TPLINK_ARCHER_C8_HTML
+    def test_arris_nvg589_html_returns_correct_name_when_enumerating_from_source(self):
+        page_html = ARRIS_NVG589_HTML
 
         self.router_enumerator.enumerate_from_source(page_html)
 
-        expected = 'TP-LINK Archer C8'
+        expected = 'arris nvg589'
         actual = self.router_enumerator.full_name
 
         self.assertEqual(expected, actual)
